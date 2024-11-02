135fb446c57ffea508de5be4b3ec12d8626fe818
Murray Groves <github@murraygrov.es>
2024-11-02 13:12:20 +0000

    Add basic lobby room

--- CSS GameJam 2024/Assets/Scripts/PlayerController.cs
+++ CSS GameJam 2024/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController: MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Camera cam;
+    public GameObject weapon;
 
     private Vector2 movement;
     private Vector2 mousePos;
@@ -16,14 +17,23 @@ public class PlayerController: MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (Mouse.current.leftButton.isPressed)
+        {
+            weapon.GetComponent<Laser>().ShootLaser();
+        } else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            weapon.GetComponent<Laser>().RemoveLaser();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        weapon.GetComponentInParent<Rigidbody2D>().MovePosition(rb.position);
         
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
+        weapon.GetComponentInParent<Rigidbody2D>().rotation = angle;
     }
 }
